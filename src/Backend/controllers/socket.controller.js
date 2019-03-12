e8192e272237c4c53ee033f3bae0349988d124f5
Klara Eserstam <klaraesr@n131-p176.eduroam.kth.se>
2019-03-12 11:10:15 +0100

    upvotes working with sockets

--- src/Backend/controllers/socket.controller.js
+++ src/Backend/controllers/socket.controller.js
@@ -4,14 +4,15 @@ const router = express.Router();
 
 module.exports = (socket, io) => {
 
-    socket.on('example', req => {
-        // model.setBookedBy(req.timeslot, req.booked_by).then(updated => {
-        //     io.sockets.emit('update', {event: 'book', timeslot: updated})
-        // })
-    });
-
+    // add a workout to the database
     socket.on('addWorkout', async req => {
         const workout = await model.makeWorkout(req.userId, req.group_training, req.sessions, req.date)
         io.sockets.emit('updateFeed')
+    });
+
+    // upvote an existing workout
+    socket.on('upvote', async req => {
+        const updated = await model.upvoteWorkout(req.workoutId)
+        io.sockets.emit('upvote', {workoutId: updated.id, upvotes: updated.likes})
     });
 }
